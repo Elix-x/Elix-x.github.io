c6e2a9631e828bcaa493e726fe24659a0c5152cb
elix-x <Elix-x@users.noreply.github.com>
2017-05-24 20:54:52 +0200

    Let's see how this looks

---
+++ Contact/contact_nof.js
@@ -0,0 +1,29 @@
+// Begin XHTML adjustment
+$(document).ready(function(){
+	if (jQuery.browser.msie && jQuery.browser.version.substr(0, 2) == "6.") {
+		$(".nof-clearfix").each(function (i) {
+			$(this).append("<div style='clear:both'/>");
+			$(this).removeClass("nof-clearfix");
+		});
+	}
+});
+
+// End XHTML adjustment
+
+// Begin Navigation Bars
+var ButtonsImageMapping = [];
+ButtonsImageMapping["NavigationBar1"] = {
+	"NavigationButton1" : { image: "../Home_Np_regular.png", rollover: "../Home_NRp_regularOver.png", w: 120, h: 56 },
+	"NavigationButton2" : { image: "../Minecraft_Np_regular.png", rollover: "../Minecraft_NRp_regularOver.png", w: 120, h: 56 },
+	"NavigationButton3" : { image: "../Contact_Hp_highlighted.png", rollover: "../Contact_HRp_highlightedOver.png", w: 120, h: 56 }
+};
+
+$(document).ready(function(){
+	$.fn.nofNavBarOptions({ navBarId: "NavigationBar1", rollover: true, autoClose: true });
+	$("#NavigationBar1").nofNavBar({isMain: true, orientation: "horizontal" });
+	$("#NavigationBar1 ul").hide();
+});
+
+
+// End Navigation Bars
+
